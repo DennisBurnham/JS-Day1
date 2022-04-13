38cd12838a4af2ead4e281f328097bb970be3ca8
Dennis Burnham <db@pear-tree.us>
2022-04-13 16:27:46 -0600

    Pie chart with custom dialog

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 import c3 from "c3"
 
 
-function loadChart ( ) {
-  const options =   {
-    bindto: '#chart',
-    data: {
-    type: "bar",
-    colors: { Apples : "purple" , Peaches : "orange" } ,
-    columns: [
-      ["Apples", 330, 100, 400, 150, 250, 400],
-      ["Peaches", 550, 10, 40, 15, 25, 333]
-    ],
-    bar: {
-      width: 30
-    }
-  },
-  };
+function loadChart ( data ) {
+  // const options =   {
+    console.log(data);
+
+  const columns = JSON.parse(data);
+  console.log(columns);
+
+  //   bindto: '#chart',
+  //   data: {
+  //   type: "bar",
+  //   colors: { Apples : "purple" , Peaches : "orange" } ,
+  //   columns: [
+  //     ["Apples", 330, 100, 400, 150, 250, 400],
+  //     ["Peaches", 550, 10, 40, 15, 25, 333]
+  //   ],
+  //   bar: {
+  //     width: 30
+  //   }
+  // },
+  // };
   
   const options2 = {
     bindto: '#piechart',
     data: {
+      onclick: function (d,element) {
+        console.log("onclick",d ) ;
+        console.log("onclick", element );
+        FileMaker.PerformScript("Get data from charts", JSON.stringify(d));
+      },
        type: "pie",
        columns: columns ,
        },
