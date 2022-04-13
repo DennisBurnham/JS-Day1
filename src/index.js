a66905ddec59060eca1a08e6a58ef0059a801c5b
Dennis Burnham <db@pear-tree.us>
2022-04-13 15:24:20 -0600

    April 12 work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3"
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
 
-//
+function loadChart ( ) {
+  const options =   {
+    bindto: '#chart',
+    data: {
+    type: "bar",
+    colors: { Apples : "purple" , Peaches : "orange" } ,
+    columns: [
+      ["Apples", 330, 100, 400, 150, 250, 400],
+      ["Peaches", 550, 10, 40, 15, 25, 333]
+    ],
+    bar: {
+      width: 30
+    }
+  },
+  };
+  
+  const options2 = {
+    bindto: '#piechart',
+    data: {
+       type: "pie",
+       columns: columns ,
+       },
+       pie: {
+        padAngle: .05
+      }
+    };
+  
+  // const chart = c3.generate(options)
+  const piechart = c3.generate(options2 ) 
+
+}
+
+
+window.loadChart = loadChart
+
+
